1685592256a1f5e820a9512ff9555a18c0c4c78c
theart <artshadow74@gmail.com>
2022-12-05 21:28:36 +0500

    fix post poll method

--- src/dto/get-poll-votes.dto.ts
+++ src/dto/get-poll-votes.dto.ts
@@ -1,9 +1,7 @@
-import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class GetPollVotesDto {
   @IsNotEmpty()
-  @Transform(({ value }) => console.log(value))
   @IsString()
   public readonly vote: string;
 
